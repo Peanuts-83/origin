99386ac0056a318249c0458e6729e0d7c7e5731d
Thomas Ranque <tranque@free.fr>
2021-05-01 16:34:57 +0200

    sql & no sql

---
+++ Exercices_DB/MongoDB/init-mongo.js
@@ -0,0 +1,12 @@
+debugger.createUser(
+    {
+        user : "peanuts",
+        pwd : "peanuts-83",
+        roles : [
+            {
+                role : "ReadWrite",
+                db : "tourPedia"
+            }
+        ]
+    }
+)
