09a2d973b14d20d30094e8a90d0585d4f0bcb43a
Peanuts-83 <tranque@free.fr>
2021-11-04 10:18:06 +0100

    env.ts

--- Exercices_JS/Angular/bookshelves/src/environments/environment.ts
+++ Exercices_JS/Angular/bookshelves/src/environments/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: false,
-  apiKey: 'AIzaSyDnHHKSCCcsXpyjxPBtSh-nQ7aZfGDIYu8',
-  authDomain: 'firstproject-aa4fd.firebaseapp.com',
-  databaseURL: 'https://firstproject-aa4fd-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'firstproject-aa4fd',
-  storageBucket: 'firstproject-aa4fd.appspot.com',
-  messagingSenderId: '998528775360',
-  appId: '1:998528775360:web:836f5754e3598d13876925',
-  measurementId: 'G-V6LL8N4N0V'
+  production: false
 }
